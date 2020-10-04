eddc6701b6d4031b7b48969bc6eba78a6e3d94cd
s81214 <hanna.tranlarsson@seb.se>
2020-10-04 17:33:24 +0200

    first commit

---
+++ TrafikBus/src/main/java/com/challenge/trafikBus/TrafikBusApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.trafikBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafikBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafikBusApplication.class, args);
+	}
+
+}
